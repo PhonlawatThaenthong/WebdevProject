7bb5f362100477b3f67cc079f75abcef3cc7276d
Sutthikarn <kimotorihana@gmail.com>
2024-02-15 14:33:50 +0700

    Home Page WIP

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,25 +1,64 @@
 import React, { useState } from 'react';
-import { Form, Input, Button, Modal, Row, Col } from 'antd';
+import { Form, Input, Button, Modal, Row, Col, Layout, Flex } from 'antd';
 import { Helmet } from 'react-helmet';
 import { useNavigate } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 
-
+const { Header, Footer, Sider, Content } = Layout;
 
 const HomeForm = () => {
     const navigate = useNavigate();
-    const [form] = Form.useForm();
-    const [submitEnabled, setSubmitEnabled] = useState(true);
-    const [showErrorModal, setShowErrorModal] = useState(false);
-    const [errorMessage, setErrorMessage] = useState('');
+
+    const headerStyle = {
+        textAlign: 'center',
+        color: '#fff',
+        height: 120,
+        paddingInline: "center",
+        lineHeight: '120x',
+        backgroundColor: '#1C3953',
+        display: 'flex',
+        justifyContent: 'left',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: '45px',
+    };
+
+    const contentStyle = {
+        textAlign: 'center',
+        lineHeight: 'calc(5000%)',
+        color: '#fff',
+        backgroundColor: '#EEEEEE',
+    };
+
+    const layoutStyle = {
+        borderRadius: 8,
+        overflow: 'hidden',
+        backgroundColor: '#EEEEEE',
+    };
+
+    const blueTextStyle = {
+        color: '#48D3FF',
+        fontWeight: 'bold',
+        fontSize: '45px',
+    };
 
     return (
-        <Row justify="center" align="middle" style={{ minHeight: '100vh' }}>
+        <Flex gap="middle" wrap="wrap" >
             <Helmet>
                 <title>HYJ - Home Page</title>
             </Helmet>
-            
-        </Row>
+
+            <Layout style={layoutStyle}>
+                <Header style={headerStyle}>
+                    <span style={blueTextStyle}>H</span>
+                    AT
+                    <span style={blueTextStyle}> Y</span>
+                    AI <span style={blueTextStyle}>J</span>ourney
+                </Header>
+                <Content style={contentStyle}> ...</Content>
+            </Layout>
+
+        </Flex>
     );
 };
 
