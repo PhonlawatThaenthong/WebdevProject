b36ba933b17ece9be517d33bf32c8dfe6799a903
phonlawat <pthaenthong@gmail.com>
2024-02-18 14:15:05 +0700

    make comfirm payment

--- server/src/api/reserve/controllers/reserve.js
+++ server/src/api/reserve/controllers/reserve.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::reserve.reserve');
+module.exports = createCoreController('api::reserve.reserve', ({ strapi }) => ({
+    
+    async confirm(ctx) {
+        const entityId = ctx.params.id;
+        try {
+
+            let item = await strapi.entityService.findOne("api::reserve.reserve", entityId, {
+                populate: '*',
+            });
+
+            if (ctx.state.user.role.name != "admin") {
+                item = await strapi.entityService.update("api::reserve.reserve", entityId, {
+                    data: {
+                        payment_status: 'True'
+                        
+                    }
+                })
+                ctx.body = { Stats: "Update" }
+
+            } 
+            else {
+                ctx.body = { Stats: "Error Confirm Payment", seenDatetime: "cannot be update" }
+            }
+
+
+        } catch (err) {
+            ctx.body = "error"
+        }
+
+    },
+})
+);
---
+++ server/src/api/reserve/routes/custom.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+    routes: [ 
+
+        {
+            method: 'POST',
+            path: '/reserve/:id/confirm',
+            handler: 'reserve.confirm'
+        },
+
+
+        
+
+    ]
+}
