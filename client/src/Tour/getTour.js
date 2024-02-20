44a335eec7388b62f63990588638156b74960068
Suchada <6610110335@psu.ac.th>
2024-02-20 23:54:29 +0700

    navigate after click select

--- client/src/Tour/getTour.js
+++ client/src/Tour/getTour.js
@@ -1,18 +1,30 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-import { useLocation } from "react-router-dom";
-import useLocalState from '../localStorage.js';
+import { useLocation, useNavigate } from "react-router-dom";
+import useLocalState from "../localStorage.js";
 
 import {
-  Card, Form, Input, Image, Button, Modal, Row, Col, Layout, Flex, Space, Popconfirm, message
+  Card,
+  Form,
+  Input,
+  Image,
+  Button,
+  Modal,
+  Row,
+  Col,
+  Layout,
+  Flex,
+  Space,
+  Popconfirm,
+  message,
 } from "antd";
-
 
 const Tour = ({ data, filterData }) => {
   const location = useLocation();
+  const navigate = useNavigate();
   const currentPage = location.pathname;
   const [selectedTourId, setSelectedTourId] = useState(null);
-  const [jwt, setjwt] = useLocalState(null, 'jwt');
+  const [jwt, setjwt] = useLocalState(null, "jwt");
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const handleOpenModal = (id) => {
@@ -23,89 +35,121 @@ const Tour = ({ data, filterData }) => {
   const getStatusColor = (status) => {
     switch (status) {
       case true:
-        return `rgba(0, 255, 0)`
+        return `rgba(0, 255, 0)`;
       case false:
-        return `rgba(255, 0, 0)`
+        return `rgba(255, 0, 0)`;
     }
   };
 
   const getStatus = (status) => {
     switch (status) {
       case true:
-        return `ว่าง`
+        return `ว่าง`;
       case false:
-        return `เต็ม`
+        return `เต็ม`;
     }
   };
 
   const handleSelect = () => {
-    console.log('select clicked!')
-  }
+    if (jwt) {
+      navigate("/payments");
+    } else {
+      navigate("/login");
+    }
+  };
 
   const handleTourDelete = async (id) => {
     try {
-      await axios.delete(
-        `http://localhost:1337/api/tours/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${jwt}`,
-          },
-        }
-      );
+      await axios.delete(`http://localhost:1337/api/tours/${id}`, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      });
 
-      window.location.href = '/'
+      window.location.href = "/";
     } catch (error) {
-      console.error('Error deleting tour:', error);
+      console.error("Error deleting tour:", error);
     }
-  }
+  };
 
   const toursToDisplay = filterData.length > 0 ? filterData : data;
 
   return (
-    <div style={{
-      display: "flex",
-      backgroundColor: "#F5F5F5"
-    }}>
+    <div
+      style={{
+        display: "flex",
+        backgroundColor: "#F5F5F5",
+      }}
+    >
       {toursToDisplay.length === 0 ? (
-        <b style={{
-          position: "absolute", 
-          top: "50%",
-          left: "50%",
-          transform: "translate(-50%, -50%)"
-        }}>
+        <b
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "50%",
+            transform: "translate(-50%, -50%)",
+          }}
+        >
           Loading Data...
         </b>
       ) : (
         toursToDisplay.map(({ id, attributes }) => (
           <Card key={id} style={{ width: 300, margin: 20, marginTop: 50 }}>
             {currentPage === "/admin" ? (
-              <Modal title={attributes.tour_name}
+              <Modal
+                title={attributes.tour_name}
                 open={isModalOpen && selectedTourId === id}
-                onCancel={() => { setIsModalOpen(false) }}
+                onCancel={() => {
+                  setIsModalOpen(false);
+                }}
                 footer={[
-                  <Button key="back" onClick={() => { setIsModalOpen(false) }}>
+                  <Button
+                    key="back"
+                    onClick={() => {
+                      setIsModalOpen(false);
+                    }}
+                  >
                     Close
                   </Button>,
                   <Popconfirm
                     title="Delete the tour"
                     description="Are you sure to delete this tour?"
-                    onConfirm={() => { setIsModalOpen(false); handleTourDelete(id) }}
+                    onConfirm={() => {
+                      setIsModalOpen(false);
+                      handleTourDelete(id);
+                    }}
                     okText="Yes"
                     cancelText="No"
                   >
                     <Button danger>Delete</Button>
                   </Popconfirm>,
-                  <Button key="submit" type="primary" onClick={() => { setIsModalOpen(false) }}>
+                  <Button
+                    key="submit"
+                    type="primary"
+                    onClick={() => {
+                      setIsModalOpen(false);
+                    }}
+                  >
                     Save
                   </Button>,
-                ]}>
-                <Image src={`https://semantic-ui.com/images/wireframe/white-image.png`}
+                ]}
+              >
+                <Image
+                  src={`https://semantic-ui.com/images/wireframe/white-image.png`}
                   preview={false}
                 />
                 <br />
-                สถานะ: <span style={{ color: getStatusColor(attributes.status) }}>
+                สถานะ:{" "}
+                <span style={{ color: getStatusColor(attributes.status) }}>
                   <b>{getStatus(attributes.status)}</b>
-                  <b> {"(" + attributes.user_amount + "/" + attributes.user_max + ")"}</b>
+                  <b>
+                    {" "}
+                    {"(" +
+                      attributes.user_amount +
+                      "/" +
+                      attributes.user_max +
+                      ")"}
+                  </b>
                 </span>
                 <br />
                 ราคา: {attributes.price} บาท / ท่าน
@@ -114,25 +158,44 @@ const Tour = ({ data, filterData }) => {
                 <br />
                 <br></br>
               </Modal>
-            ) : (  // VVVVVVVV THIS IS NON ADMIN MODAL PLEASE EDIT THIS ONLY /////////////////////////////////////////////////////
-              <Modal title={attributes.tour_name}
+            ) : (
+              // VVVVVVVV THIS IS NON ADMIN MODAL PLEASE EDIT THIS ONLY /////////////////////////////////////////////////////
+              <Modal
+                title={attributes.tour_name}
                 open={isModalOpen && selectedTourId === id}
-                onCancel={() => { setIsModalOpen(false) }}
+                onCancel={() => {
+                  setIsModalOpen(false);
+                }}
                 footer={[
-                  <Button key="back" onClick={() => { setIsModalOpen(false) }}>
+                  <Button
+                    key="back"
+                    onClick={() => {
+                      setIsModalOpen(false);
+                    }}
+                  >
                     Close
                   </Button>,
                   <Button key="submit" type="primary" onClick={handleSelect}>
                     Select
                   </Button>,
-                ]}>
-                <Image src={`https://semantic-ui.com/images/wireframe/white-image.png`}
+                ]}
+              >
+                <Image
+                  src={`https://semantic-ui.com/images/wireframe/white-image.png`}
                   preview={false}
                 />
                 <br />
-                สถานะ: <span style={{ color: getStatusColor(attributes.status) }}>
+                สถานะ:{" "}
+                <span style={{ color: getStatusColor(attributes.status) }}>
                   <b>{getStatus(attributes.status)}</b>
-                  <b> {"(" + attributes.user_amount + "/" + attributes.user_max + ")"}</b>
+                  <b>
+                    {" "}
+                    {"(" +
+                      attributes.user_amount +
+                      "/" +
+                      attributes.user_max +
+                      ")"}
+                  </b>
                 </span>
                 <br />
                 ราคา: {attributes.price} บาท / ท่าน
@@ -142,14 +205,19 @@ const Tour = ({ data, filterData }) => {
                 <br></br>
               </Modal>
             )}
-            <Image src={`https://semantic-ui.com/images/wireframe/white-image.png`}
+            <Image
+              src={`https://semantic-ui.com/images/wireframe/white-image.png`}
               preview={false}
             />
             <b style={{ fontSize: "18px" }}>{attributes.tour_name}</b>
             <br />
-            สถานะ: <span style={{ color: getStatusColor(attributes.status) }}>
+            สถานะ:{" "}
+            <span style={{ color: getStatusColor(attributes.status) }}>
               <b>{getStatus(attributes.status)}</b>
-              <b> {"(" + attributes.user_amount + "/" + attributes.user_max + ")"}</b>
+              <b>
+                {" "}
+                {"(" + attributes.user_amount + "/" + attributes.user_max + ")"}
+              </b>
             </span>
             <br />
             ราคา: {attributes.price} บาท / ท่าน
@@ -158,9 +226,26 @@ const Tour = ({ data, filterData }) => {
             <br />
             <br></br>
             {currentPage === "/admin" ? (
-              <Button type="primary" onClick={() => handleOpenModal(id)} style={{ display: "block", margin: "0 auto", backgroundColor: "#DE3163" }}>Edit</Button>
-            ) : (  // VVVVVVVV THIS IS NON ADMIN MODAL PLEASE EDIT THIS ONLY /////////////////////////////////////////////////////
-              <Button type="primary" onClick={() => handleOpenModal(id)} style={{ display: "block", margin: "0 auto" }}>ดูเพิ่มเติม</Button>
+              <Button
+                type="primary"
+                onClick={() => handleOpenModal(id)}
+                style={{
+                  display: "block",
+                  margin: "0 auto",
+                  backgroundColor: "#DE3163",
+                }}
+              >
+                Edit
+              </Button>
+            ) : (
+              // VVVVVVVV THIS IS NON ADMIN MODAL PLEASE EDIT THIS ONLY /////////////////////////////////////////////////////
+              <Button
+                type="primary"
+                onClick={() => handleOpenModal(id)}
+                style={{ display: "block", margin: "0 auto" }}
+              >
+                ดูเพิ่มเติม
+              </Button>
             )}
           </Card>
         ))
