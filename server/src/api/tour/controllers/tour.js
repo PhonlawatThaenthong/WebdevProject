48f5e50746d70ca81ae7500a3b16e875741591ce
Sutthikarn <kimotorihana@gmail.com>
2024-02-22 15:16:39 +0700

    Fixed Custom Action not working correctly

--- server/src/api/tour/controllers/tour.js
+++ server/src/api/tour/controllers/tour.js
@@ -12,11 +12,22 @@ module.exports = createCoreController('api::tour.tour', ({ strapi }) => ({
         const entityId = ctx.params.id;
         try {
             let target_Tour = await strapi.entityService.findOne('api::tour.tour', entityId)
-            target_Tour = await strapi.entityService.update('api::tour.tour', entityId, {
-                data: {
-                    user_amount: (target_Tour.user_amount || 0) + 1
-                }
-            })
+
+            if ((target_Tour.user_amount + 1) >= target_Tour.user_max) {
+                target_Tour = await strapi.entityService.update('api::tour.tour', entityId, {
+                    data: {
+                        user_amount: (target_Tour.user_amount || 0) + 1,
+                        status: false
+                    }
+                })
+            } else {
+                target_Tour = await strapi.entityService.update('api::tour.tour', entityId, {
+                    data: {
+                        user_amount: (target_Tour.user_amount || 0) + 1
+                    }
+                })
+            }
+
             ctx.body = {
                 status: "YES",
                 message: "Action Completed!"
