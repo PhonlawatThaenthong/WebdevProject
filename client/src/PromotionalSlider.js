f2dff25534ec3433637e920f79af2c195b539502
Suchada <6610110335@psu.ac.th>
2024-02-29 22:56:04 +0700

    fix slider image load

--- client/src/PromotionalSlider.js
+++ client/src/PromotionalSlider.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import './index.css'
+import './index.css';
 
 const PromotionalSlider = ({ images }) => {
+  const [imagesLoad, setImagesLoad] = useState(false);
+
+  useEffect(() => {
+    const preloadImages = () => {
+      const imagePromise = images.map((image) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = image;
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      });
+
+      Promise.all(imagePromise)
+        .then(() => setImagesLoad(true))
+        .catch((error) => console.error('error preloading images:', error));
+    };
+
+    preloadImages();
+  }, [images]);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -13,18 +34,19 @@ const PromotionalSlider = ({ images }) => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-  
   };
 
   return (
-    <div className="promotional-slider-wrapper">
-      <Slider {...settings}>
-        {images.map((image, index) => (
-          <div key={index} className="slider-item">
-            <img src={image} alt={`Promotion ${index + 1}`} />
-          </div>
-        ))}
-      </Slider>
+    <div className={`promotional-slider-wrapper ${imagesLoad ? 'show' : 'loading'}`}>
+      {imagesLoad && (
+        <Slider {...settings}>
+          {images.map((image, index) => (
+            <div key={index} className="slider-item">
+              <img src={image} alt={`Promotion ${index + 1}`} />
+            </div>
+          ))}
+        </Slider>
+      )}
     </div>
   );
 };
