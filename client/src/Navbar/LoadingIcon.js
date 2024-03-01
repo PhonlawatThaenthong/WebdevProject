c2b2b109e877955a06e4f9f1ad0e72e42d7f7874
Sutthikarn <kimotorihana@gmail.com>
2024-03-02 01:30:13 +0700

    No Data Element

--- client/src/Navbar/LoadingIcon.js
+++ client/src/Navbar/LoadingIcon.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { LoadingOutlined } from '@ant-design/icons';
-import { Spin } from 'antd';
+import { Spin, Empty } from 'antd';
+
 const LoadingIcon = () => (
-  <Spin
-    indicator={
-      <LoadingOutlined
-        style={{
-          fontSize: 75,
-        }}
-        spin
-      />
-    }
-  />
+  <div>
+    <Empty image={Empty.PRESENTED_IMAGE_SIMPLE} />
+    <Spin
+      indicator={
+        <LoadingOutlined
+          style={{
+            fontSize: 75,
+          }}
+          spin
+        />
+      }
+    />
+  </div>
 );
+
 export default LoadingIcon;
