6a32132f7980b4a7c9aac415dc46ef6a80bda607
Phum Khongkaeo <coldmagic86@gmail.com>
2024-03-04 00:43:42 +0700

    fix this code is gone after push so I fixed it in Payment XD

--- client/src/Payment.js
+++ client/src/Payment.js
@@ -130,7 +130,7 @@ const Payment = () => {
     };
 
     return (
-        <Flex gap="middle" wrap="wrap">
+        <Flex gap="middle" wrap="wrap" style={{ backgroundColor: "#F5F5F5" }}>
             <Helmet>
                 <title>HYJ - Home Page</title>
             </Helmet>
