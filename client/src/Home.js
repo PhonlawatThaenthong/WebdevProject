9b731c725d91851c6a46491663abbdfd895f6f2f
Pattarapon <peternus.2005@gmail.com>
2024-03-04 15:35:31 +0700

    Comment Button

--- client/src/Home.js
+++ client/src/Home.js
@@ -291,7 +291,7 @@ const HomeForm = () => {
 
         >
           <div className="scroll_button">
-            <button onClick={handleScrollToElement}>Scroll to Element</button>
+            {/* <button onClick={handleScrollToElement}>Scroll to Element</button> */}
             <button>Scroll to Element</button>
           </div>
 
