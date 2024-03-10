18bf48814185e987fe71c4d7c270b03677b7085f
phonlawat <pthaenthong@gmail.com>
2024-03-10 20:09:15 +0700

    test numberofpeople when reserve tour

---
+++ client/src/Tour/Nop.test.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import Tour from './getTour';
+
+describe('Tour Component', () => {
+  test('increments numberOfPeople when input changes', () => {
+    const { getByLabelText } = render(<Tour data={/number/} filterData={[]} />);
+    
+    const numberOfPeopleInput = getByLabelText('Number of People:');
+    fireEvent.change(numberOfPeopleInput, { target: { value: '2' } });
+
+    expect(numberOfPeopleInput.value).toBe('2');
+  });
+});
